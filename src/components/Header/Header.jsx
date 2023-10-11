ee28ed2eeabad2c7288f0eb1b3fb26baddedd44a
Junior Hirwa <iamhirwejr@gmail.com>
2023-10-11 22:00:08 +0200

    style: update the header styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,13 +2,13 @@ export default function Header() {
   const links = ["collections", "Men", "Women", "About", "Contact"];
   return (
     <>
-      <div className="flex justify-between items-center w-[80%] mx-auto mt-2 hover:cursor-pointer font-KumbRegular">
+      <div className="flex justify-between items-center w-[80%] mx-auto mt-2 font-KumbRegular">
         <div className="flex gap-4 items-center md:gap-20 ">
           <div className="md:hidden">
-            <img src="/images/icon-menu.svg" alt="" />
+            <img src="/images/icon-menu.svg"/>
           </div>
           <div>
-            <img src="/images/logo.svg" alt="" />
+            <img src="/images/logo.svg" className="cursor-pointer"/>
           </div>
           <ul className=" hidden md:flex  gap-10 text-sm  text-darkishGreyBlue ">
             {links.map((link) => (
@@ -22,7 +22,7 @@ export default function Header() {
 
         <div className="flex gap-3 md:gap-16 items-center ">
           <div>
-            <img src="/images/icon-cart.svg" alt="cart icon" />
+            <img src="/images/icon-cart.svg" alt="cart icon" className="cursor-pointer"/>
           </div>
           <div className=" w-[30px] lg:w-[50px]">
             <img
