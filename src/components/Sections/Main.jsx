17aa4423f08579b275ea0c692ce2be6754367f33
Junior Hirwa <iamhirwejr@gmail.com>
2023-10-11 22:31:09 +0200

    feat: accept the isVisible state from the app component

--- src/components/Sections/Main.jsx
+++ src/components/Sections/Main.jsx
@@ -1,12 +1,13 @@
 import ImagesSection from "../imagesSection/ImagesSection"
 import Aside from "./Aside"
 
-export default function Main() {
+export default function Main(props) {
+  
   return (
     <>
         <section className="w-3/5 mx-auto flex item-center  justify-around mt-10 gap-20">
             <ImagesSection/>
-            <Aside/> 
+            <Aside isVisible={props.isVisible}/> 
         </section>
     </>
   )
